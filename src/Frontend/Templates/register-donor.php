3aaca91545d88c42b7cbfd7c80cf3ad42c287bd4
sakhawat9 <sakhawathossain7969@gmail.com>
2024-09-23 14:45:23 +0600

    update

--- src/Frontend/Templates/register-donor.php
+++ src/Frontend/Templates/register-donor.php
@@ -15,25 +15,30 @@ if (!defined('ABSPATH')) {
   die(esc_html(IDONATE_ALERT_MSG));
 }
 
+
 use ThemeAtelier\Idonate\Helpers\Helpers;
+
+if (is_user_logged_in() && !current_user_can('donor')) {
+  wp_safe_redirect(home_url());
+}
+
 
 Helpers::idonate_custom_header();
 ?>
 <div class="section-padding">
   <div class="ta-container">
     <?php
-    if (!is_user_logged_in()) {
-      echo do_shortcode('[register-donor]');
-    } else {
+
+    if (is_user_logged_in() &&  current_user_can('donor')) {
       echo '
       <div class="register_success_donor">
-        <h3>' . esc_html('Thank you for registering as an donor!', 'idonate') . '</h3>
-        
+        <h3>' . esc_html('Thank you for registering as a donor!', 'idonate') . '</h3>
         <a href="' . esc_url(home_url() . '/dashboard') . '">' . esc_html('Go to Dashboard', 'idonate') . '</a>
       </div>
       ';
+    } else {
+      echo do_shortcode('[register-donor]');
     }
-
     ?>
   </div>
 </div>
