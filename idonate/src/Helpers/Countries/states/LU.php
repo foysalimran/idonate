97c562df227868ca80e27685f6e0de79df52a8b2
Foysal Imran <fimran2011@gmail.com>
2024-10-01 15:53:36 +0600

    update

--- idonate/src/Helpers/Countries/states/LU.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-global $states;
-
-$states['LU'] = [
-    'DIEKIRCH'     => __( 'Diekirch', 'idonate' ),
-    'GREVENMACHER' => __( 'Grevenmacher', 'idonate' ),
-    'LUXEMBOURG'   => __( 'Luxembourg', 'idonate' ),
-];
